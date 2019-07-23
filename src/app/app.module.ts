dbedce8ad91a5e2d90d5b0607e0ab3bc027f4a9a
Chu Hung Lin <mouse170@gmail.com>
2019-07-23 10:03:58 +0800

    切割元件 header, article, footer, tags, 移除Spec.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ArticleComponent } from './article/article.component';
+import { TagsComponent } from './tags/tags.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArticleComponent,
+    TagsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
